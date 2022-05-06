bb6485723db67080f26ebea707e8ae04258fb154
chien-liu <liu_chien@gapp.nthu.edu.tw>
2022-05-06 11:20:36 +0200

    Add example code to change input font color

---
+++ js/input_string.js
@@ -0,0 +1,10 @@
+function compare_answer(id, answer) {
+    const input = document.getElementById(id);
+
+    if (input.value == answer) {
+        input.style.color = "green";
+    } else {
+        input.style.color = "red";
+    }
+    input.style.fontWeight = "bold";
+}
